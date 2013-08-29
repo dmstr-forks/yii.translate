953b622fa04d010c7b9ec2409fe76629b7ff1998
Christopher Stebe <chris@stebe.eu>
2013-08-29 16:53:04 +0200

    add public param $useApplicationLanguage [->getLanguage() , ->setLanguage() ]

--- components/MPTranslate.php
+++ components/MPTranslate.php
@@ -9,6 +9,10 @@ class MPTranslate extends CApplicationComponent{
      * */
     public $defaultLanguage=null;
     /**
+     * @var string $useApplicationLanguage
+     * */
+    public $useApplicationLanguage = false;
+    /**
      * @var $dialogOptions options of the dialog
      * */
     public $dialogOptions=array(
@@ -185,29 +189,35 @@ class MPTranslate extends CApplicationComponent{
      * 
      * @return string
      */
-    function getLanguage(){
-        $key=self::ID;
-        if(($language=@$this->_cache['language'])!==null)
-            return $language;
-        elseif(Yii::app()->getSession()->contains($key))
-            $language=Yii::app()->getSession()->get($key);
-        elseif(isset($_POST[$key]) && !empty($_POST[$key]))
-            $language=$_POST[$key];
-        elseif(isset($_GET[$key]) && !empty($_GET[$key]))
-            $language=$_GET[$key];
-        else
-            $language=Yii::app()->getRequest()->getPreferredLanguage();
-        
-        if(!key_exists($language,$this->acceptedLanguages)){
-            if($language===Yii::app()->sourceLanguage)
-                $language=$this->defaultLanguage;
-            elseif(strpos($language,"_")!==false){
-                $language=substr($language,0,2);
-                if(!key_exists($language,$this->acceptedLanguages))
-                    $language=$this->defaultLanguage;
+    function getLanguage()
+    {
+        $key = self::ID;
+
+        if ($this->useApplicationLanguage === true) {
+            return Yii::app()->language;
+        } else {
+            if (($language = @$this->_cache['language']) !== null)
+                return $language;
+            elseif (Yii::app()->getSession()->contains($key))
+                $language = Yii::app()->getSession()->get($key);
+            elseif (isset($_POST[$key]) && !empty($_POST[$key]))
+                $language = $_POST[$key];
+            elseif (isset($_GET[$key]) && !empty($_GET[$key]))
+                $language = $_GET[$key];
+            else
+                $language = Yii::app()->getRequest()->getPreferredLanguage();
+
+            if (!key_exists($language, $this->acceptedLanguages)) {
+                if ($language === Yii::app()->sourceLanguage)
+                    $language = $this->defaultLanguage;
+                elseif (strpos($language, "_") !== false) {
+                    $language = substr($language, 0, 2);
+                    if (!key_exists($language, $this->acceptedLanguages))
+                        $language = $this->defaultLanguage;
+                }
             }
+            return $language;
         }
-        return $language;
     }
     /**
      * 
@@ -223,11 +233,18 @@ class MPTranslate extends CApplicationComponent{
         if($language===null)
             $language=$this->getLanguage();
         
-        $this->_cache['language']=$language;
+        elseif ($this->useApplicationLanguage === true) {
+            return Yii::app()->language;
+        }
+        else {
         
-        Yii::app()->getSession()->add(self::ID,$language);
-        Yii::app()->setLanguage($language);
-        return $language;
+            $this->_cache['language']=$language;
+
+            Yii::app()->getSession()->add(self::ID,$language);
+            Yii::app()->setLanguage($language);
+
+            return $language;
+        }
     }
     /**
      * generates a dropdown containing all accepted languages
